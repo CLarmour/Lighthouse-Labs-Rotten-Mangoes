9d2d1c2828fc1e7920d8dc6b710ebfac3e1ffd54
CLarmour <connorlarmour@outlook.com>
2015-11-24 22:36:18 +0000

    added validation errors to _form

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,7 +6,7 @@ class Movie < ActiveRecord::Base
   validates :description, presence: true
   validates :poster_image_url, presence: true
   validates :release_date, presence: true
-  validates :release_date_is_in_the_past
+  validate :release_date_is_in_the_past
 
   protected
 
@@ -15,5 +15,5 @@ class Movie < ActiveRecord::Base
       errors.add(:release_date, "should be in the past") if release_date > Date.today
     end
   end
-  
+
 end
