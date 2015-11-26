f6d5538ebb670ebe30c4f4e958c2a8e100546708
CLarmour <connorlarmour@outlook.com>
2015-11-26 00:32:49 +0000

    Added image uploader from user's files. Added small tweaks to improve user experience

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,12 +6,17 @@ class Movie < ActiveRecord::Base
   validates :director, presence: true
   validates :runtime_in_minutes, numericality: {only_integer: true}
   validates :description, presence: true
-  validates :poster_image_url, presence: true
   validates :release_date, presence: true
   validate :release_date_is_in_the_past
+  validate :must_have_image
+
+  mount_uploader :image, ImageUploader
 
   def review_average
-    reviews.sum(:rating_out_of_ten)/reviews.size
+    if reviews.sum(:rating_out_of_ten) == 0
+      "No reviews yet"
+    else reviews.sum(:rating_out_of_ten)/reviews.size
+    end
   end
 
   protected
@@ -19,6 +24,12 @@ class Movie < ActiveRecord::Base
   def release_date_is_in_the_past
     if release_date.present?
       errors.add(:release_date, "should be in the past") if release_date > Date.today
+    end
+  end
+
+  def must_have_image
+    if image.blank? && poster_image_url.blank?
+      errors.add(:image, "Please add image either by URL or uploader")
     end
   end
 
